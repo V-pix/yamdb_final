a9f5077d6f1603fb0d05c0cfc7c23cfbea784310
Veronika <veronika.shakalova@yandex.ru>
2022-08-19 00:44:21 +0300

    Первый коммит

---
+++ api_yamdb/api/views.py
@@ -0,0 +1,146 @@
+from django.db.models import Avg
+from django.shortcuts import get_object_or_404
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters, mixins, permissions, status, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
+
+from reviews.models import Category, Genre, User, Review, Title
+from .utils import confirmation_generator
+from .filters import TitleFilter
+from .permissions import (AdminPermission,
+                          IsAdminOrReadOnly,
+                          IsAuthorAdminModerOrReadOnly)
+from .serializers import (ObtainTokenSerializer,
+                          RegistrationSerializer,
+                          UserSerializer,
+                          CategorySerializer,
+                          GenreSerializer,
+                          TitleSerializer,
+                          ReviewSerializer,
+                          CommentSerializer)
+
+
+class GetPostDeleteViewSet(mixins.CreateModelMixin, mixins.DestroyModelMixin,
+                           mixins.ListModelMixin, viewsets.GenericViewSet):
+    pass
+
+
+class UserRegistrationView(APIView):
+    def post(self, request):
+        serializer = RegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            username = request.data.get('username')
+            confirmation_generator(username)
+            return Response(
+                serializer.data,
+                status=status.HTTP_200_OK
+            )
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
+
+class AuthTokenView(APIView):
+    def post(self, request):
+        serializer = ObtainTokenSerializer(data=request.data)
+        if serializer.is_valid():
+            username = serializer.data['username']
+            confirmation_code = serializer.data['confirmation_code']
+            user = get_object_or_404(User, username=username)
+            if confirmation_code != user.confirmation_code:
+                return Response(
+                    serializer.errors,
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+            token = RefreshToken.for_user(user)
+            return Response(
+                {'token': str(token.access_token)},
+                status=status.HTTP_200_OK
+            )
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (AdminPermission,)
+    lookup_field = "username"
+
+    @action(
+        methods=["GET", "PATCH"],
+        detail=False,
+        permission_classes=[permissions.IsAuthenticated],
+    )
+    def me(self, request):
+        serializer = UserSerializer(request.user)
+        if request.method == "PATCH":
+            serializer = UserSerializer(
+                request.user,
+                data=request.data,
+                partial=True
+            )
+            if serializer.is_valid():
+                serializer.validated_data['role'] = request.user.role
+                serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class CategoryViewSet(GetPostDeleteViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [IsAdminOrReadOnly]
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter)
+    search_fields = ('name',)
+    lookup_field = 'slug'
+
+
+class GenreViewSet(GetPostDeleteViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    permission_classes = [IsAdminOrReadOnly]
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter)
+    search_fields = ('name',)
+    lookup_field = 'slug'
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+    queryset = Title.objects.all().annotate(
+        Avg('reviews__score')).order_by('name')
+
+    permission_classes = [IsAdminOrReadOnly]
+    serializer_class = TitleSerializer
+    filterset_class = TitleFilter
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    serializer_class = ReviewSerializer
+    permission_classes = [IsAuthorAdminModerOrReadOnly]
+
+    def get_queryset(self):
+        title = get_object_or_404(Title, id=self.kwargs.get('title_id'))
+        return title.reviews.all()
+
+    def perform_create(self, serializer):
+        title = get_object_or_404(Title, id=self.kwargs.get('title_id'))
+        serializer.save(author=self.request.user, title=title)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthorAdminModerOrReadOnly]
+
+    def get_queryset(self):
+        review = get_object_or_404(Review, id=self.kwargs.get('review_id'))
+        return review.comments.all()
+
+    def perform_create(self, serializer):
+        review = get_object_or_404(Review, id=self.kwargs.get('review_id'))
+        serializer.save(author=self.request.user, review=review)
---
+++ api_yamdb/reviews/models.py
@@ -0,0 +1,158 @@
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+
+
+class User(AbstractUser):
+    USER = 'user'
+    MODER = 'moderator'
+    ADMIN = 'admin'
+
+    USER_ROLES = (
+        (MODER, 'Модератор'),
+        (ADMIN, 'Администратор'),
+        (USER, 'Пользователь'),
+    )
+
+    bio = models.TextField(
+        'Биография',
+        blank=True,
+        null=True
+    )
+    role = models.CharField(
+        'Роль пользователя',
+        max_length=50,
+        default=USER,
+        choices=USER_ROLES
+    )
+    email = models.EmailField(
+        'Почта',
+        unique=True
+    )
+    confirmation_code = models.CharField(
+        'Код авторизации',
+        max_length=15,
+        blank=True,
+        null=True
+    )
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['username', 'email'], name='unique_user')
+        ]
+
+    def __str__(self):
+        return self.username
+
+    @property
+    def is_admin(self):
+        return self.is_superuser or self.role == "admin" or self.is_staff
+
+    @property
+    def is_moder(self):
+        return self.role == 'moderator'
+
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length=256,
+        verbose_name="Категория",
+        help_text="Проверьте название категории",
+    )
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField(
+        max_length=256,
+        verbose_name="Жанр",
+        help_text="Проверьте название жанра",
+    )
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Title(models.Model):
+    name = models.CharField(
+        max_length=200,
+        verbose_name="Произведение",
+        help_text="Проверьте название произведения",
+    )
+    year = models.PositiveSmallIntegerField(
+        verbose_name="Год выпуска", help_text="Проверьте название год выпуска"
+    )
+    description = models.TextField(verbose_name="Описание произведения")
+    genre = models.ManyToManyField(
+        Genre,
+        related_name="titles",
+        blank=True,
+        verbose_name="Жанр произведения",
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        related_name="titles",
+        blank=True,
+        null=True,
+        verbose_name="Категория произведения",
+    )
+
+    class Meta:
+        ordering = ["-year"]
+
+    def __str__(self):
+        return self.name
+
+
+class Review(models.Model):
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+        related_name="reviews",
+    )
+    text = models.TextField("Текст", help_text="Отзыв")
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="reviews",
+        verbose_name="Автор",
+    )
+    score = models.SmallIntegerField(
+        verbose_name="Оценка",
+        validators=[MinValueValidator(1), MaxValueValidator(10)],
+    )
+    pub_date = models.DateTimeField("Дата публикации", auto_now_add=True)
+
+    class Meta:
+        ordering = ["-pub_date"]
+        constraints = [
+            models.UniqueConstraint(
+                fields=["author", "title"], name="unique_review"
+            )
+        ]
+
+    def __str__(self):
+        return self.text[:15]
+
+
+class Comment(models.Model):
+    review = models.ForeignKey(
+        Review, on_delete=models.CASCADE, related_name="comments"
+    )
+    text = models.TextField("Текст", help_text="Комментарий")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="comments"
+    )
+    pub_date = models.DateTimeField("Дата публикации", auto_now_add=True)
+
+    class Meta:
+        ordering = ["-pub_date"]
+
+    def __str__(self):
+        return self.text
